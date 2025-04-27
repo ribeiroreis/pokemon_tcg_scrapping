01d5f5a00401154abc03431ed95101938ab07426
Filipe Ribeiro <52297807+ribeiroreis@users.noreply.github.com>
2025-04-27 11:37:54 -0300

    Update webscrap_liga_pokemon.py

--- webscrap_liga_pokemon.py
+++ webscrap_liga_pokemon.py
@@ -22,51 +22,20 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 # URL pública do Google Sheets em formato CSV
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSPXtRVhFqD8ADjOKjI3h-d81gmK0UdVTYfIKqe05N_QxvLxCiaEaua3bdv_oYX4c7fM606qG9dSHlz/pub?gid=464076543&single=true&output=csv'
 
-# Carregar a planilha
-sheet_data = pd.read_csv(spreadsheet_url)
-urls = sheet_data['url'].tolist()
-colecoes = sheet_data.iloc[:, 0].tolist()
+# Acesse a página
+url = "https://www.pichau.com.br/placa-de-video-gigabyte-radeon-rx-7600-xt-gaming-oc-16gb-gddr6-128-bit-gv-r76xtgaming-oc-16gd?gad_source=1&gclid=CjwKCAjwq7fABhB2EiwAwk-YbHKPu2eP6MOxnn_AytWtx8r9Rt6YmYykZ3CJqHlnV4PF9xeKjW19qRoCrHsQAvD_BwE"
+driver.get(url)
 
-# Função para extrair dados da tabela
-def get_table_data(driver, table_xpath):
-    table = driver.find_element(By.XPATH, table_xpath)
-    rows = table.find_elements(By.TAG_NAME, "tr")
-    return [[col.text for col in row.find_elements(By.TAG_NAME, "td")] for row in rows]
+# Espera a página carregar (ajuste o tempo se necessário)
+time.sleep(5)
 
-data = []
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# Tenta encontrar o preço utilizando o XPath
+try:
+    price_element = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div[4]/div[1]/div/div[1]/div[2]/div[2]")
+    price = price_element.text
+    print(f"Preço encontrado: {price}")
+except Exception as e:
+    print(f"Erro ao encontrar o preço: {e}")
 
-for colecao, url in zip(colecoes, urls):
-    driver.get(url)
-    try:
-        # Espera até o botão de expandir estoque aparecer
-        button_xpath = '//*[@id="card-estoque"]/div[1]/div[2]/div/div[3]/div[1]/img'
-        WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, button_xpath))
-        ).click()
-
-        # Espera até a tabela aparecer
-        table_xpath = '//*[@id="card-estoque"]/div[3]/div'
-        WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, table_xpath))
-        )
-
-        table_data = get_table_data(driver, table_xpath)
-        for row in table_data:
-            data.append({'colecao': colecao, 'dados_tabela': row, 'extraction_time': current_time})
-    except Exception as e:
-        print(f"Erro ao processar {colecao}: {e}")
-        # Salvar screenshot pra debug
-        screenshot_path = f'screenshot_{colecao.replace(" ", "_")}.png'
-        driver.save_screenshot(screenshot_path)
-
+# Fecha o navegador
 driver.quit()
-
-# Salvar dados como CSV
-df = pd.DataFrame(data)
-print(df)
-
-os.makedirs('raw', exist_ok=True)
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_path = f'raw/colecoes_{timestamp}.csv'
-df.to_csv(output_path, index=False)
